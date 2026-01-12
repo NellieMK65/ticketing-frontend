ff1166c87838ef9300fdf7d7bf6ae10b3190d520
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2026-01-12 09:42:06 +0300

    wip: setup login

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter } from 'react-router';
-import { RouterProvider } from 'react-router/dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-import './index.css';
-import Home from './pages/home';
+import "./index.css";
+import Home from "./pages/home";
+import Login from "./pages/login";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Home />,
-	},
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
